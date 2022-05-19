9abb79511945c58fed18d4ff0771d87c1a60dc2e
quixt <88290402+quixt@users.noreply.github.com>
2022-05-19 13:14:49 -0700

    Created two more ruins pages

--- docs/js/data_href.js
+++ docs/js/data_href.js
@@ -1,7 +1,7 @@
     var dataHrefs = document.querySelectorAll('[data-href]');
     console.log(dataHrefs);
     dataHrefs.forEach(function(dataHref) {
-
+        dataHref.setAttribute('title', dataHref.getAttribute('data-href'));
         dataHref.addEventListener('click', function(e) {
             e.preventDefault();
             window.location.href = this.getAttribute('data-href');
--- docs/js/info.js
+++ docs/js/info.js
@@ -12,3 +12,18 @@ function setDesc(el) {
 function empty() {
     info.style.display = "none";
 }
+
+var info2 = document.getElementsByClassName("info")[1];
+var desc2 = info2.querySelector(".info-desc");
+var sec2 = info2.querySelector("section");
+var descImg2 = info2.querySelector(".info-img");
+
+function setDesc2(el) {
+    sec2.style.display = "block";
+    desc2.innerHTML = el.getAttribute("data-desc");
+    descImg2.src = el.getAttribute("data-src");
+}
+
+function empty2() {
+    info.style.display = "none";
+}
